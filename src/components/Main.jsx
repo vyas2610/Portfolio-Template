0afa896f69201a04f04cb8a7a9c85bd1ca033594
vyas2610 <deepakvyas261092@gmail.com>
2024-11-08 13:31:51 +0530

    Updates Main Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,5 @@
-import React from 'react';
+import React from "react";
 
-const MainContent = () => 
-    {
-        return
-        (
-            <div>
-                <h2>Experience</h2>
-                <div className='container mx-auto bg-red-300'>
-                    <p>hello</p>
-                </div>
-            </div>
-        )
-    }
-
-export default MainContent;
+export const Main = () => {
+  return <div>Main</div>;
+};
