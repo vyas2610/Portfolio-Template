fd5791e619d2b3b27dd61fdfd02eb189f9c654f1
AswanipiyusOP <aswanipiyush1234@gmail.com>
2024-11-12 19:43:34 +0530

    modified the data and added experience section.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // React Router
 // import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import MainContent from './components/Main';
+import Header from "./components/Header";
+import { Main } from "./components/Main";
 
 function App() {
   return (
     <div className='bg-slate-50'>
       <div className="containers px-4">
-        
           <Header />
-          <MainContent />
+          <Main />
       </div>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
-const MainContent = () => 
+import Avatar from '@mui/material/Avatar';
+export const Main = () => 
     {
-        return
-        (
-            <div>
-                <h2>Experience</h2>
-                <div className='container mx-auto bg-red-300'>
-                    <p>hello</p>
-                </div>
+        return(
+            <div className='bg-white'>
+                <h2 className='text-slate-700 pt-9 px-14 pb-4 text-lg font-semibold'>Experience</h2>
+                    <div className="w-3/4 justify-self-center rounded-md bg-gray-200 ">
+                        <p className='pt-5 pl-16 pb-4 text-xs italic text-gray-500'>2020-Present</p>
+                        <Avatar alt="QONTO" src="./images/qonto_logo.png" className='mx-6 '/>
+                    </div>
             </div>
-        )
-    }
+        );
+    };
 
-export default MainContent;
+ 
