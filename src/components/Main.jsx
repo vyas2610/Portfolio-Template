c7c7739b1018199e377eba33261cd680c3a85094
AswanipiyusOP <aswanipiyush1234@gmail.com>
2024-11-14 15:54:58 +0530

    performing events on slider image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 export const Main = () => {
+  
   return (
     <div className="bg-white">
       <h2 className="text-slate-700 pt-9 px-14 pb-4 text-lg font-semibold">
         Experience
       </h2>
-      <div className="w-3/4 justify-self-center rounded-md bg-gray-200 ">
-        <p className="pt-5 pl-16 pb-4 text-xs italic text-gray-500">
-          2020-Present
-        </p>
-        <Avatar alt="QONTO" src="./images/qonto_logo.png" className="mx-6 " />
+      <div className="lg:w-3/4 sm:w-5/6 justify-self-center rounded-md bg-gray-200 ">
+            <p className="pt-5 pl-16 pb-4 text-xs italic text-gray-500">
+            2020-Present
+            </p>
+        <div className="flex  hover:cursor-pointer">
+            <Avatar alt="QONTO" src="./images/qonto_logo.png" className="mx-3 " />
+            <h3 className="text-sm md:text-base lg:text-lg font-semibold">Senior Developer and VP of product at QONTO</h3>
+        </div>
       </div>
     </div>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Avatar from "@mui/material/Avatar";
 
 const Header = () => {
+  const [isHovered, setIsHovered] = useState(true);
+
+ const handleMouseOver = () => setIsHovered(false);
+  const handleMouseOut = () => setIsHovered(true);
+
+
   return (
     <div className="text-center bg-white ">
       <img
@@ -22,28 +28,28 @@ const Header = () => {
       </p>
       <button className="btn mt-4">Available for Work</button>
       <div className=" h-50 w-4/5 m-auto mt-2 ">
-        <div className=" md:flex md:flex-row  gap-5 sm:flex sm:flex-col  sm:justify-self-center">
-          <div className="lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40">
+        <div className="md:flex md:flex-row  gap-5 sm:flex sm:flex-col  sm:justify-self-center">
+
+          <div className={`lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40 ${isHovered ? 'img1' : ''}`} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
             <img
               src="./images/slider3.jpg"
               alt=""
-              className="w-full h-full rounded-lg  img-1 my-5 "
+              className="w-full h-full rounded-lg my-5 "
             />
           </div>
-
-          <div className="lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40 sm:hover:scale-110 sm:hover:ease-in-out">
+          <div className={`lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40 ${isHovered ? 'img2' : ''}`} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
             <img
               src="./images/slider2.jpg"
               alt=""
-              className="w-full h-full rounded-lg  img-2 my-5 "
+              className="w-full h-full rounded-lg my-5 "
             />
           </div>
 
-          <div className="lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40">
-            <img
+          <div className={`lg:w-60 lg:h-48 md:w-52 md:h-40 sm:w-60 sm:h-40 ${isHovered ? 'img1' : ''}`} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
+            <img 
               src="./images/slider1.jpg"
               alt=""
-              className="w-full h-full rounded-lg  img-1 my-5 "
+              className="w-full h-full rounded-lg my-5 "
             />
           </div>
         </div>
