88847bd2180baa72bd7fff8ad3089618988f0942
vyas2610 <deepakvyas261092@gmail.com>
2024-11-14 12:04:28 +0530

    UI Changes & Responsive Web UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // React Router
 // import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './App.css';
+import "./App.css";
 import Header from "./components/Header";
 import { Main } from "./components/Main";
 
 function App() {
   return (
-    <div className='bg-slate-50 max-w-full'>
-      <div className="containers  px-4">
-          <Header />
-          <Main />
+    <div className="bg-slate-50 max-w-full">
+      <div className="containers  md:px-4 sm:px-10">
+        <Header />
+        <Main />
       </div>
     </div>
   );
